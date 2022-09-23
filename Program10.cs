30b5602412f337bcabed7bae5aa880c6db248f5f
mixxxas777 <113127885+mixxxas777@users.noreply.github.com>
2022-09-23 13:13:58 +0300

    Add files via upload

---
+++ Program10.cs
@@ -0,0 +1,11 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.Clear();
+int x = new Random().Next(100,999);
+Console.WriteLine(x);
+int temp = x/10;
+int temp2 = temp%10;
+Console.WriteLine(temp2);
