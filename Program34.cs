8182ccd60955d0c916614487d42d8856968a84e2
mixxxas777 <113127885+mixxxas777@users.noreply.github.com>
2022-10-06 18:33:28 +0300

    Add files via upload

---
+++ Program34.cs
@@ -0,0 +1,38 @@
+﻿//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2;
+
+int size = ReadInt("Введите длину массива: ");
+int [] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+int result = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] % 2 == 0)
+    {
+        result ++;   
+    }
+    else result = result + 0;
+}
+Console.WriteLine($"В массиве {result} четных числа");
+void FillArrayRandomNumbers(int [] array) //Заполнение массива
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(99, 1000);
+    }
+}
+void PrintArray(int[] array) //Вывод массива на экран
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+int ReadInt(string message)  //Функция ввода
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
