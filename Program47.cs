1df567d44fb1ae78404ee45ebfa8ff0311c217cd
mixxxas777 <113127885+mixxxas777@users.noreply.github.com>
2022-10-10 13:29:37 +0300

    Add files via upload

---
+++ Program47.cs
@@ -0,0 +1,39 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+//m = 3, n = 4.
+//0,5 7 -2 -0,2
+//1 -3,3 8 -9,9
+//8 7,8 -7,1 9
+
+Console.Clear();
+Console.WriteLine("введите количество строк m =");
+int m = Convert.ToInt32(Console.ReadLine()); ;
+Console.WriteLine("введите количество столбцов n =");
+int n = Convert.ToInt32(Console.ReadLine()); ;
+double[,] numbers = new double [m, n];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+void FillArrayRandomNumbers(double[,] array) //Заполнение массива
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
+
+void PrintArray(double[,] array)//Вывод массива на экран
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("| ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("|");
+        Console.WriteLine("");
+    }
+}
---
+++ Program52.cs
@@ -0,0 +1,51 @@
+﻿//Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.Clear();
+Console.WriteLine("введите количество строк m =");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов n =");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[m, n];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+for (int j = 0; j < numbers.GetLength(1); j++)
+{
+    double result = 0;
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        result = (result + numbers[i, j]);
+    }
+    result = result / m;
+    Console.Write($"  {result = Math.Round(result,1)}  ");
+}
+Console.WriteLine();
+void FillArrayRandomNumbers(int[,] array) //Заполнение массива
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)//Вывод массива на экран
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("| ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "     ");
+        }
+        Console.Write("|");
+        Console.WriteLine("");
+    }
+}
